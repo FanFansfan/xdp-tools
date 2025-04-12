dd89afaa6206b0b1b9300d64ec14dbe3fa011b50
shangyatsen <429839446@qq.com>
2025-04-12 22:57:40 +0800

    二层转发修改

--- xdp-forward/xdp_forward.bpf.c
+++ xdp-forward/xdp_forward.bpf.c
@@ -12,6 +12,11 @@
 
 #define IPV6_FLOWINFO_MASK              bpf_htons(0x0FFFFFFF)
 
+struct bpf_crypto_ctx *bpf_crypto_ctx_create(const struct bpf_crypto_params *params,
+	__u32 params__sz, int *err) __ksym;
+struct bpf_crypto_ctx *bpf_crypto_ctx_acquire(struct bpf_crypto_ctx *ctx) __ksym;
+void bpf_crypto_ctx_release(struct bpf_crypto_ctx *ctx) __ksym;
+
 struct {
 	__uint(type, BPF_MAP_TYPE_DEVMAP_HASH);
 	__uint(key_size, sizeof(int));
@@ -19,15 +24,10 @@ struct {
 	__uint(max_entries, 64);
 } xdp_tx_ports SEC(".maps");
 
-/* from include/net/ip.h */
-static __always_inline int ip_decrease_ttl(struct iphdr *iph)
+struct connection
 {
-	__u32 check = (__u32)iph->check;
-
-	check += (__u32)bpf_htons(0x0100);
-	iph->check = (__sum16)(check + (check >= 0xFFFF));
-	return --iph->ttl;
-}
+	struct bpf_crypto_ctx __kptr * ctx;
+};
 
 static __always_inline int xdp_fwd_flags(struct xdp_md *ctx, __u32 flags)
 {
@@ -56,7 +56,59 @@ static __always_inline int xdp_fwd_flags(struct xdp_md *ctx, __u32 flags)
 
 		if (iph->ttl <= 1)
 			return XDP_PASS;
+		if (iph->protocol == IPPROTO_IPIP) { // unlikely，客户端上行是少的
+			// todo 根据外层IP决定使用哪个加密隧道出去
+			struct iphdr *inner_ip = iph + 1;
+			if (inner_ip + 1 > data_end) {
+				return XDP_DROP;
+			}
 
+			// 按封包格式加密直接网卡丢回去
+			return XDP_TX;
+		}
+
+		if (iph->protocol == IPPROTO_TCP) { // 加密隧道是TCP
+			// 服务器端，unlikely，如果是传入端口，转发到用户态AF_XDP处理
+			// 客户端，收到的是握手回复的话，转发到用户态AF_XDP处理
+
+			// 路由器通过IPIP ICMP ping 加密隧道对端时，应该期待ICMP也从IPIP隧道返回。
+			// 其他情况下，不走IPIP隧道直接回给局域网
+
+			// 查询是否隧道下行包
+			// 1. 封包解密
+
+			// 2. 重置为内层隧道的类型，查找邻居，直接转发
+			iph = iph + 1;
+			h_proto = bpf_htons(ETH_P_IP);
+		} else {
+			// 不是上行，也不是下行流量，不管
+			return XDP_PASS;
+		}
+	} else if (h_proto == bpf_htons(ETH_P_IPV6)) {
+
+		ip6h = data + nh_off;
+		if (ip6h + 1 > data_end)
+			return XDP_DROP;
+
+		if (ip6h->hop_limit <= 1)
+			return XDP_PASS;
+
+		// todo 支持上行IPv6 封装
+		// if (ip6h->nexthdr == IPPROTO_IPV6) {
+		// }
+
+		if (ip6h->nexthdr == IPPROTO_TCP) {
+
+		} else {
+			return XDP_PASS;
+		}
+	} else {
+		return XDP_PASS;
+	}
+
+	// 到这里是下行隧道的转发了
+	// 由于nat放到了隧道对端去做，这里的ipv4/ipv6 dst都已经是局域网了
+	if (h_proto == bpf_htons(ETH_P_IP)) {
 		fib_params.family	= AF_INET;
 		fib_params.tos		= iph->tos;
 		fib_params.l4_protocol	= iph->protocol;
@@ -68,14 +120,6 @@ static __always_inline int xdp_fwd_flags(struct xdp_md *ctx, __u32 flags)
 	} else if (h_proto == bpf_htons(ETH_P_IPV6)) {
 		struct in6_addr *src = (struct in6_addr *) fib_params.ipv6_src;
 		struct in6_addr *dst = (struct in6_addr *) fib_params.ipv6_dst;
-
-		ip6h = data + nh_off;
-		if (ip6h + 1 > data_end)
-			return XDP_DROP;
-
-		if (ip6h->hop_limit <= 1)
-			return XDP_PASS;
-
 		fib_params.family	= AF_INET6;
 		fib_params.flowinfo	= *(__be32 *)ip6h & IPV6_FLOWINFO_MASK;
 		fib_params.l4_protocol	= ip6h->nexthdr;
@@ -85,7 +129,8 @@ static __always_inline int xdp_fwd_flags(struct xdp_md *ctx, __u32 flags)
 		*src			= ip6h->saddr;
 		*dst			= ip6h->daddr;
 	} else {
-		return XDP_PASS;
+		// 隧道内没有IP协议外的协议
+		return XDP_DROP;
 	}
 
 	fib_params.ifindex = ctx->ingress_ifindex;
@@ -117,13 +162,9 @@ static __always_inline int xdp_fwd_flags(struct xdp_md *ctx, __u32 flags)
 		 *  cannot pass map_type 14 into func bpf_map_lookup_elem#1:
 		 */
 		if (!bpf_map_lookup_elem(&xdp_tx_ports, &fib_params.ifindex))
-			return XDP_PASS;
+			return XDP_PASS; // 上面注释说如果邻居表ARP为空，需要PASS上去做ARP解析
 
-		if (h_proto == bpf_htons(ETH_P_IP))
-			ip_decrease_ttl(iph);
-		else if (h_proto == bpf_htons(ETH_P_IPV6))
-			ip6h->hop_limit--;
-
+		// 不递减ttl了
 		__builtin_memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
 		__builtin_memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
 		return bpf_redirect_map(&xdp_tx_ports, fib_params.ifindex, 0);
@@ -142,6 +183,40 @@ SEC("xdp")
 int xdp_fwd_fib_direct(struct xdp_md *ctx)
 {
 	return xdp_fwd_flags(ctx, BPF_FIB_LOOKUP_DIRECT);
+}
+
+struct tunnel_args {
+    int prog_fd;
+    unsigned int hid;
+    int retval;
+
+	char cipher[128];
+	__u8 key[256];
+};
+
+SEC("syscall")
+int set_tunnel(struct tunnel_args *args)
+{
+	// 修改map
+	struct bpf_crypto_ctx *cctx;
+    struct bpf_crypto_params params = {
+        .type = "skcipher",
+        .key_len = 1,
+        .authsize = 1,
+    };
+	int err = 0;
+
+	struct bpf_crypto_ctx *cctx;
+	__builtin_memcpy(&params.algo, args->cipher, sizeof(args->cipher));
+    __builtin_memcpy(&params.key, args->key, sizeof(args->key));
+    cctx = bpf_crypto_ctx_create(&params, sizeof(params), &err);
+	if (!cctx) {
+		return err;
+	}
+
+	(void)cctx;
+
+    return 0;
 }
 
 char _license[] SEC("license") = "GPL";
